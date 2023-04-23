ede811793f75f7a49273c492b2fd4863b4d33a2f
Willian Monoga <bytewareteam@gmail.com>
2023-04-22 22:17:25 -0500

    fix: center the container

--- src/styled-components/layout.styled.component.ts
+++ src/styled-components/layout.styled.component.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
   width: 80VW;
+  margin: 0 auto;
 
   @media only screen and (max-width: 1176px) {
     padding: 12px;
